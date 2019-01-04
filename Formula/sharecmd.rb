ee63b63d6a53d62ec08ba029120dff40991c0eda
goreleaserbot <goreleaser@carlosbecker.com>
2019-01-04 14:40:42 +0100

    Brew formula update for sharecmd version v0.0.80

--- Formula/sharecmd.rb
+++ Formula/sharecmd.rb
@@ -1,9 +1,9 @@
 class Sharecmd < Formula
   desc "Share your files using Cloudproviders with just one command"
   homepage "https://github.com/mschneider82/sharecmd"
-  url "https://github.com/mschneider82/sharecmd/releases/download/v0.0.75/sharecmd_0.0.75_Darwin_x86_64.tar.gz"
-  version "0.0.75"
-  sha256 "69f39d3a4e63114b85d93881967ad1e17061a01eebf0af8a238b9835121062a7"
+  url "https://github.com/mschneider82/sharecmd/releases/download/v0.0.80/sharecmd_0.0.80_Darwin_x86_64.tar.gz"
+  version "0.0.80"
+  sha256 "a1caa6644915174a69383ff8172f4af641bd4378461be215300d469c1a0a9b68"
 
   def install
     bin.install "share"
