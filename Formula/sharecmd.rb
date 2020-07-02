267fa99c102a8f360edb6d48837ee43297425484
goreleaserbot <goreleaser@carlosbecker.com>
2020-07-02 09:15:18 +0000

    Brew formula update for sharecmd version v0.0.106

--- Formula/sharecmd.rb
+++ Formula/sharecmd.rb
@@ -2,9 +2,27 @@
 class Sharecmd < Formula
   desc "Share your files using Cloudproviders with just one command"
   homepage "https://github.com/mschneider82/sharecmd"
-  url "https://github.com/mschneider82/sharecmd/releases/download/v0.0.105/sharecmd_0.0.105_Darwin_x86_64.tar.gz"
-  version "0.0.105"
-  sha256 "a437d20a567cbc78c031ed9f6f5bc5004eff0f7509b559199b4916d4ce0045ec"
+  version "0.0.106"
+  bottle :unneeded
+
+  if OS.mac?
+    url "https://github.com/mschneider82/sharecmd/releases/download/v0.0.106/sharecmd_0.0.106_Darwin_x86_64.tar.gz"
+    sha256 "64b6e62011d465f21a161f0413da46ded8c9157ea6f8d2a820a61bd92f9349eb"
+  elsif OS.linux?
+    if Hardware::CPU.intel?
+      url "https://github.com/mschneider82/sharecmd/releases/download/v0.0.106/sharecmd_0.0.106_Linux_x86_64.tar.gz"
+      sha256 "44add7be72f7d9bca7ace743f2ed707b883326aee854fda87dc984fbbd7ddfc3"
+    end
+    if Hardware::CPU.arm?
+      if Hardware::CPU.is_64_bit?
+        url "https://github.com/mschneider82/sharecmd/releases/download/v0.0.106/sharecmd_0.0.106_Linux_arm64.tar.gz"
+        sha256 "3ec3e866245b54a9cca605beb5056334752aa1ad457d1855e5cf3f88bc71ee2f"
+      else
+        url "https://github.com/mschneider82/sharecmd/releases/download/v0.0.106/sharecmd_0.0.106_Linux_armv6.tar.gz"
+        sha256 "451f91b087a01ce60d282448bc740b81affd86c3637e96ff2872cc8017611589"
+      end
+    end
+  end
 
   def install
     bin.install "share"
