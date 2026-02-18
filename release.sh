d6b2f4bab6b064a92f5181f511b1bd1ed3db380e
Matthias Schneider <ms@wck.biz>
2026-02-18 17:21:08 +0100

    workflow

--- release.sh
+++ release.sh
@@ -5,6 +5,7 @@ count=$(git log --oneline | wc -l)
 tag="v0.0.$count"
 
 git tag -a "$tag" -m "release"
-git push origin "$tag"
+git push
+git push --tags
 
 echo "Tag $tag pushed. GitHub Actions will build and release."
